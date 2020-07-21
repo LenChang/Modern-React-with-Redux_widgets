6cf9eed02be2ac1c8a10eaa31807138c230b5057
Len Chang <lamgo.chang@gmail.com>
2020-07-21 17:09:40 +0800

    162 to 165 - Create Dropdown Arch.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 const items = [
   { title: "What is React?", content: "React is a front end js framework" },
@@ -10,10 +11,23 @@ const items = [
   },
 ];
 
+const options = [
+  { label: "The Color Red", value: "red" },
+  { label: "The Color Green", value: "green" },
+  { label: "The Shade of Blue", value: "blue" },
+];
+
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Search></Search>
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      ></Dropdown>
+      {/* <Search></Search> */}
       {/* <Accordion items={items} /> */}
     </div>
   );
