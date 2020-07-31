2c082b885aedd85c91c9c3c03139bd9b6e58c930
Len Chang <lamgo.chang@gmail.com>
2020-07-31 18:55:24 +0800

    176. to 183. - Add Translate Widget - Use Google Translate API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import Accordion from "./components/Accordion";
-import Search from "./components/Search";
+
 import Dropdown from "./components/Dropdown";
+import Translate from "./components/Translate";
 
 const items = [
   { title: "What is React?", content: "React is a front end js framework" },
@@ -18,21 +18,9 @@ const options = [
 ];
 
 export default () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropdown, setShowDropdown] = useState(true);
-
   return (
     <div>
-      <button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </button>
-      {showDropdown ? (
-        <Dropdown
-          selected={selected}
-          onSelectedChange={setSelected}
-          options={options}
-        ></Dropdown>
-      ) : null}
+      <Translate />
     </div>
   );
 };
