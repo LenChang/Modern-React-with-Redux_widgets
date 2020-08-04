ada9c3eafb97a949d897697bcb67bc7f770e33c7
Len Chang <lamgo.chang@gmail.com>
2020-08-04 14:35:53 +0800

    192. Implementing a Header for Navigation

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Translate from "./components/Translate";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   { title: "What is React?", content: "React is a front end js framework" },
@@ -25,6 +26,7 @@ export default () => {
 
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items}></Accordion>
       </Route>
