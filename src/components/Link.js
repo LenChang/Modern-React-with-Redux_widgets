b1f167b0070eaf00f6048e5df5531ec6c1ca0e48
Len Chang <lamgo.chang@gmail.com>
2020-08-05 17:12:23 +0800

    198. Handling Command Clicks - event.metakey - event.ctrlKey

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,11 @@ import React from "react";
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    // Holding Command Clicks
+    if (event.metakey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault();
     window.history.pushState({}, "", href);
 
